63447c326ffc85f0eacb7a73f37fb457f076f044
Justin Young <justin_dean_young@yahoo.com>
2026-05-15 23:41:38 -0400

    removed UIButton, playing with camera and circle colliders

--- src/main.ts
+++ src/main.ts
@@ -13,25 +13,26 @@ const game = new Engine({
 
 await game.start();
 
-game.add(
-  new UIButton({
-    name: "myButton",
-    idleText: "Click Me",
-    pos: vec(100, 100),
-    width: 100,
-    height: 50,
-  }),
-);
+// game.add(
+//   new UIButton({
+//     name: "myButton",
+//     idleText: "Click Me",
+//     pos: vec(100, 100),
+//     width: 100,
+//     height: 50,
+//   }),
+// );
 
 let camera = game.currentScene.camera;
 camera.zoom = 1.25;
-camera.pos = vec(400, 300);
+camera.pos = vec(100, 100);
 
 class testScreenElement extends ScreenElement {
   constructor() {
     super({
       name: "testScreenElement",
       pos: vec(400, 200),
+      // radius: 50,
       width: 100,
       height: 100,
       color: Color.Red,
